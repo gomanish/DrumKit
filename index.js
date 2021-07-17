683f66dc876b5395f9644cf5dd89f47fe906f758
Manish Kumar <krmanish2101@gmail.com>
2021-07-17 16:56:17 +0530

    minor update in js file

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-window.addEventListener('keydown',function (e){
+function playing(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return; // stop the function from runnging all together
     audio.currentTime = 0 //rewind to start
     audio.play();
     key.classList.add('playing');
+    setTimeout(function(){
+        key.classList.remove('playing');
+    },80);
+}
+window.addEventListener('keydown',playing);
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(element => {
+    element.addEventListener('click', function(){
+        var value = this.getAttribute('data-key');
+        const audio = document.querySelector(`audio[data-key="${value}"]`);
+        const key = document.querySelector(`.key[data-key="${value}"]`);
+        audio.currentTime = 0 //rewind to start
+        audio.play();  
+        key.classList.add('playing');
+        setTimeout(function(){
+            key.classList.remove('playing');
+        },80);
+    });
 });
  
-const keys = document.querySelectorAll('.key');
-
-function removeTranstion(e){
-    if(e.propertyName !== 'transform') return; //skip if it not a transition
-    this.classList.remove('playing');
-}
-
-keys.forEach(element => element.addEventListener('transitionend',removeTranstion));
